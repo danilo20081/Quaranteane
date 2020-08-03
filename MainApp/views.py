c16ce9f47e9157f4cc952a35923359e19bc3089d
McHacker09 <percihermz123@gmail.com>
2020-08-03 10:53:27 +0800

    Added responsive front page

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homeView(requests):
+    context = {
+
+    }
+    return render(requests, 'MainApp/home.html', context)
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\boss\desktop\daily task\projects\quaranteane station\quaranteane\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
